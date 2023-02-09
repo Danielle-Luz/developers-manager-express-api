6ccbb0369e7c1e06c8222ca2b8e78d82f90e3549
Danielle-Luz <daniln2209@gmail.com>
2023-02-09 00:01:16 -0300

    feat: add create developer request

--- src/callbacks/requests.ts
+++ src/callbacks/requests.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
+import { database } from "../database";
+import { iMessage } from "../interfaces";
 
 export namespace requests {
-  export const createDeveloper = (req: Request, res: Response) => {
+  export const createDeveloper = async (req: Request, res: Response) => {
     const { body: newDeveloper } = req;
 
-    
+    try {
+      const createdDeveloper = await database.createDeveloper(newDeveloper);
+
+      return res.status(201).send(createdDeveloper);
+    } catch (error) {
+      const errorObject = error as Error;
+
+      const errorMessage: iMessage = {
+        message: "Failed to insert the data in the database",
+      };
+
+      console.error(errorObject.stack);
+
+      return res.status(500).send(errorMessage);
+    }
   };
 }
