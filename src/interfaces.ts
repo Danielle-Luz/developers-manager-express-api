f56b2dcc64810aa827981b8ff13a82abb30f1b12
Danielle-Luz <daniln2209@gmail.com>
2023-02-11 10:30:00 -0300

    feat: create projects technologies interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -37,6 +37,32 @@ export interface iProject {
   [key: string]: string | number | Date | null;
 }
 
+interface Project {
+  project_id: number;
+  project_name: string;
+  description: string;
+  estimated_time: string;
+  repository: string;
+  start_date: Date;
+  end_date: Date | null;
+  developer_id: number;
+  technology_id: number;
+  technology_name: string;
+}
+
+interface iProjectJoinTechnologies {
+  project_id: number;
+  project_name: string;
+  project_description: string;
+  project_estimated_time: string;
+  project_repository: string;
+  project_start_date: string;
+  project_end_date: string | null;
+  project_developer_id: number | null;
+  technology_id: number | null;
+  technology_name: string | null;
+}
+
 export interface iId {
   developer_info_id?: number | undefined;
   developer_id?: number | undefined;
